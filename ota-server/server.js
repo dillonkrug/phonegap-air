6267c2c6f3b3d4525f0834bfc437076499302e20
dillonkrug <dpkrug@gmail.com>
2014-12-03 12:22:03 -0600

    fix formatting

--- ota-server/server.js
+++ ota-server/server.js
@@ -1,100 +1,97 @@
 /**
-* A simple server that serves the contents of the www folder
-*/
+ * A simple server that serves the contents of the www folder
+ */
 
-var finalhandler = require('finalhandler')
-  , serveStatic = require('serve-static')
-  , http = require('http')
-  , path = require('path')
-  , fs = require('fs')
-  , async = require('async')
-  , crypto = require('crypto')
-  , wwwDir = path.join(__dirname, 'www')
-  , appPath = path.join(wwwDir, 'app.js')
-  , serve = serveStatic(wwwDir)
-  , server
-  , buildManifest
-  , sendManifest
-  , cachedManifest
+var finalhandler = require('finalhandler'),
+	serveStatic = require('serve-static'),
+	http = require('http'),
+	path = require('path'),
+	fs = require('fs'),
+	async = require('async'),
+	crypto = require('crypto'),
+	wwwDir = path.join(__dirname, 'www'),
+	appPath = path.join(wwwDir, 'app.js'),
+	serve = serveStatic(wwwDir),
+	server, buildManifest, sendManifest, cachedManifest;
 
 // Replace app.js placeholder so the apps know where they pulled from
-fs.readFile(appPath, function (err, data) {
-  if (err) throw err
+fs.readFile(appPath, function(err, data) {
+	if (err) throw err;
 
-  var replaced = data.toString().replace(/{{environment}}/, process.env.NODE_ENV)
+	var replaced = data.toString().replace(/{{environment}}/, process.env.NODE_ENV);
 
-  fs.writeFile(appPath, replaced, function (err) {
-    if(err) throw err
-  })
-})
+	fs.writeFile(appPath, replaced, function(err) {
+		if (err) throw err;
+	});
+});
 
 sendManifest = function _sendManifest(res) {
-  var manifest = JSON.parse(cachedManifest)
+	var manifest = JSON.parse(cachedManifest);
 
-  manifest.message = 'The version updates every second when not in production'
+	manifest.message = 'The version updates every second when not in production';
 
-  // Pin the production version so that we can test the redundant update logic
-  if(process.env.NODE_ENV == 'production') {
-    manifest.version = '1.1.0'
-  }
-  else {
-    manifest.version = '1.1.' + Math.round(Date.now()/1000)
-  }
+	// Pin the production version so that we can test the redundant update logic
+	if (process.env.NODE_ENV == 'production') {
+		manifest.version = '1.1.0';
+	} else {
+		manifest.version = '1.1.' + Math.round(Date.now() / 1000);
+	}
 
-  manifest = JSON.stringify(manifest)
+	manifest = JSON.stringify(manifest);
 
-  res.writeHead(200, {
-    'content-length': manifest.length
-  , 'content-type': 'application/json'
-  })
+	res.writeHead(200, {
+		'content-length': manifest.length,
+		'content-type': 'application/json'
+	});
 
-  res.end(manifest)
-}
+	res.end(manifest);
+};
 
-buildManifest = function _buildManifest (req, res, done) {
-  if(cachedManifest) {
-    sendManifest(res)
-  }
-  else {
-    fs.readdir(wwwDir, function (err, files) {
-      if(err) return done(err)
+buildManifest = function _buildManifest(req, res, done) {
+	if (cachedManifest) {
+		sendManifest(res);
+	} else {
+		fs.readdir(wwwDir, function(err, files) {
+			if (err) return done(err);
 
-      cachedManifest = {files: {}, assets: []}
+			cachedManifest = {
+				files: {},
+				assets: []
+			};
 
-      async.map(files, function (filename, next) {
-        var filepath = path.join(wwwDir, filename)
+			async.map(files, function(filename, next) {
+				var filepath = path.join(wwwDir, filename);
 
-        fs.readFile(filepath, function (err, data) {
-          if(err) return next(err)
+				fs.readFile(filepath, function(err, data) {
+					if (err) return next(err);
 
-          cachedManifest.files[filename] = {
-            checksum: crypto.createHash('md5').update(data).digest('hex')
-          , destination: filename
-          , source: '/' + filename
-          }
+					cachedManifest.files[filename] = {
+						checksum: crypto.createHash('md5').update(data).digest('hex'),
+						destination: filename,
+						source: '/' + filename
+					};
 
-          next()
-        })
-      }, function (err) {
-        if(err) return done(err)
+					next();
+				});
+			}, function(err) {
+				if (err) return done(err);
 
-        cachedManifest = JSON.stringify(cachedManifest)
+				cachedManifest = JSON.stringify(cachedManifest);
 
-        sendManifest(res)
-      })
-    })
-  }
-}
+				sendManifest(res);
+			});
+		});
+	}
+};
 
 server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res)
+	var done = finalhandler(req, res);
 
-  if(require('url').parse(req.url).pathname == '/manifest.json') {
-    buildManifest(req, res, done)
-  }
-  else {
-    serve(req, res, done)
-  }
-})
+	if (require('url').parse(req.url).pathname == '/manifest.json') {
+		buildManifest(req, res, done);
+	} else {
+		serve(req, res, done);
+	}
+});
 
-server.listen(process.env.PORT || 8080)
+server.listen(process.env.PORT || 8080);
--- ota-server/www/app.js
+++ ota-server/www/app.js
@@ -1,10 +1,11 @@
+document.addEventListener('deviceReady', function() {
+	var metaInfo = '{{environment}} app',
+		metaInfoNode = document.createTextNode(metaInfo);
+	document.getElementById('loading').className = 'hidden';
+	document.getElementById('loaded').className = '';
+	document.getElementById('meta').appendChild(metaInfoNode);
+}, false);
 
-document.addEventListener('deviceReady', function () {
-  var metaInfo = '{{environment}} app'
-    , metaInfoNode = document.createTextNode(metaInfo)
-  document.getElementById('loading').className = 'hidden'
-  document.getElementById('loaded').className = ''
-  document.getElementById('meta').appendChild(metaInfoNode)
-}, false)
-
-window.shouldAllowOTADevTools = function () { return true }
+window.shouldAllowOTADevTools = function() {
+	return true;
+};
